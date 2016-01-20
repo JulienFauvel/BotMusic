160d46f520e37c4fa349fab89676c538072df17a
Julien Fauvel <jul.fauv@gmail.com>
2016-01-20 12:18:20 +0100

    chargement info video + restructuration

--- src/musicbot.js
+++ src/musicbot.js
@@ -1,16 +1,22 @@
 'use strict';
 
 var DiscordClient = require('discord.io');
+var YoutubeSong = require('./youtubesong.js');
 var auth = require('../auth.json');
+var http = require('http');
+var fs = require('fs');
+var URL = require('url');
+
+var audioStream = null;
+var currentSong = null;
+var queue = Array();
 
 var bot = new DiscordClient({
   autorun: true,
   email: auth.email,
   password: auth.password
-})
+});
 
-var currentSong = null;
-var queue = Array();
 
 bot.on('ready', function(rawEvent) {
   console.log(bot.username + " connected (" + bot.id + ")");
@@ -18,23 +24,45 @@ bot.on('ready', function(rawEvent) {
 
 bot.on('message', function(user, userID, channelID, message, rawEvent) {
   var cmd = message.toLowerCase().split(" ")[0];
-  console.log(message);
-  console.log(cmd);
+
   switch (cmd) {
     case "!come":
     case "!came": {
       joinChannel(user, userID, channelID);
     } break;
+
+    case "!addsong": {
+      var url = message.toLowerCase().split(" ")[1];
+      queue.push(new YoutubeSong(url, user, userID));
+    } break;
+
+    case "!skip": {
+      //TODO: Ajouter un skip si plus de 50% des personnes veulent changer
+    } break;
+
+    case "!reset": {
+      reset();
+    } break;
+
+    case "!stop": {
+      stop();
+    } break;
   }
 });
 
+//reset the queue
+function reset() {
+  queue.length = 0
+}
 
-function joinChannel(user, userID, channelID) {
+//Stop the audio
+function stop() {
+  stream.stopAudioFile();
+}
 
-  console.log('userID = ' + userID);
-
+//Return the voice channel where the user is
+function findVoiceChannelIdWhereUserIs(userID) {
   var voiceChannel = null;
-
   for(var s in bot.servers) {
     for(var uID in bot.servers[s].members) {
       if(uID == userID) {
@@ -43,23 +71,21 @@ function joinChannel(user, userID, channelID) {
     }
   }
 
+  return voiceChannel;
+}
+
+function joinChannel(user, userID, channelID) {
+  var voiceChannel = findVoiceChannelIdWhereUserIs(userID);
+
   if(voiceChannel != null) {
     bot.joinVoiceChannel(voiceChannel, function () {
-      bot.getAudioContext({ channel: voiceChannel, stereo: true}, function(stream) {
+      bot.getAudioContext({channel: voiceChannel, stereo: true}, function(stream) {
+          audioStream = stream;
           stream.playAudioFile('test.mp3'); //To start playing an audio file, will stop when it's done.
 
-
-          //stream.stopAudioFile(); //To stop an already playing file
-          // stream.once('fileEnd', function() {
-          //     //Do something now that file is done playing. This event only works for files.
-          // });
-          // // OR
-          // stream.send(rawPCMBuffer) //Piping a stream that the backend can execute the method .read(1920) on.
-          //
-          // //Experimental
-          // stream.on('incoming', function(ssrc,buffer) {
-          //     //Handle a buffer of PCM data.
-          // });
+          stream.once('fileEnd', function() {
+            console.log('Audio file ended');
+          });
       });
     });
 
@@ -75,3 +101,13 @@ function sendMessage(message, channelID) {
     message: bot.fixMessage(message)
   });
 }
+
+function debug() {
+  console.log("currentSong :");
+  console.log(currentSong);
+
+  console.log("queue :");
+  console.log(queue);
+}
+
+setInterval(debug, 2000);
--- src/youtubesong.js
+++ src/youtubesong.js
@@ -1,10 +1,83 @@
-class YoutubeSong {
+'use strict';
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-  constructor(url) {
+module.exports = function YoutubeSong(url, user, userID) {
+  this.url = url;
+  this.user = user;
+  this.userID = userID;
 
+  YoutubeSong.prototype.getTitleById = function(id) {
+    var infoUrl = 'http://youtube.com/get_video_info?video_id=' + id;
+    var tmpFile = fs.createWriteStream('./tmp/youtube_video_info.tmp');
+
+    var request = http.get(infoUrl, function(argument) {
+      response.pipe(tmpFile);
+      file.on('finish', function() {
+        file.close(readInfoFile(file));
+      });
+    });
+  };
+
+  //Return the id of a Youtube video's url
+  YoutubeSong.prototype.getIdFromUrl = function(url) {
+    var videoId = url.split('v=')[1];
+    var ampersandPosition = videoId.indexOf('&');
+    if(ampersandPosition != -1) {
+      videoId = videoId.substring(0, ampersandPosition);
+    }
+
+    return videoId;
   }
 
-  static getIdByUrl(url) {
+  YoutubeSong.prototype.getTitleFromId = function(id) {
+    var filePath = './tmp/youtube_video_info.tmp';
+    var infoUrl = 'http://youtube.com/get_video_info?video_id=' + id;
 
+    console.log(infoUrl);
+
+    var tmpFile = fs.createWriteStream(filePath);
+
+    var request = http.get(infoUrl, function(response) {
+      response.pipe(tmpFile);
+
+      tmpFile.on('finish', function() {
+        tmpFile.close(function (filePath) {
+          console.log("Filepath : " + filePath);
+
+          fs.readFile(filePath, 'utf8', function(err, data) {
+            if(err) {
+              console.error('File ' + filePath + ' can\'t be find');
+            } else {
+              console.log(data);
+              var parsedData = url.parse(data);
+              console.log(parsedData);
+              this.title = parsedData.title;
+            }
+          }); //readFile
+        }); //Close
+      }); //On finish
+    }); //Request GET
   }
+
+  YoutubeSong.prototype.readInfoFile = function (file) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      if(err) {
+        console.error('File ' + file + ' can\'t be find');
+      } else {
+        console.log(data);
+        var parsedData = url.parse(data);
+        console.log(parsedData);
+        this.title = parsedData.title;
+      }
+    });
+  }
+
+  this.id = this.getIdFromUrl(this.url);
+  this.getTitleFromId(this.id);
+
+  // var downloadFile = function() {
+  //   //TODO
+  // }
 }
